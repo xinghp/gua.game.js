e09529a661503f1995f2c206af087b9e8f98ca55
mr.xing <chenshuang9604@163.com>
2021-07-12 23:07:45 +0800

    1

--- my/打飞机/01/scene/main/scene.js
+++ my/打飞机/01/scene/main/scene.js
@@ -1,44 +1,58 @@
 class Scene extends GuaScene {
+  constructor(gua) {
+    super(gua);
+    this.init();
+  }
 
-    constructor(gua) {
-        super(gua)
-        this.init()
+  init = () => {
+    // 每帧移动 帧一张
+    this.bgSpeed = 2;
+    // 依次下移两个的临界标志  移动一个需要imgH/speed帧
+    this.skipCount = 250;
+    this.bgs = [];
+    for (var i = 0; i < 2; i++) {
+      var b = new GuaImage(imageFromPath("img/bg.png"), 0, -500 * i);
+      this.bgs.push(b);
     }
 
-    init = () => {
-        this.bg = new GuaImage(imageFromPath('img/bg.png'), 0, 0)
-        this.bgSpeed = 3
-        this.player = new Player()
+    this.player = new Player();
 
-        this.gua.registerAction('a', () => {
-            this.player.moveLeft()
-        })
-        this.gua.registerAction('d', () => {
-            this.player.moveRight()
-        })
-        this.gua.registerAction('w', () => {
-            this.player.moveTop()
-        })
-        this.gua.registerAction('s', () => {
-            this.player.moveBottom()
-        })
+    this.gua.registerAction("a", () => {
+      this.player.moveLeft();
+    });
+    this.gua.registerAction("d", () => {
+      this.player.moveRight();
+    });
+    this.gua.registerAction("w", () => {
+      this.player.moveTop();
+    });
+    this.gua.registerAction("s", () => {
+      this.player.moveBottom();
+    });
+  };
+  update() {
+    if (window.paused) {
+      // 暂停功能，不更新状态，一直绘制一个状态就是暂停
+      return;
     }
-    update() {
-
-        if (window.paused) {
-            // 暂停功能，不更新状态，一直绘制一个状态就是暂停
-            return
-        }
-        this.bg.y += this.bgSpeed
+    // 依次下移n个，再一次性上移n个
+    this.skipCount--;
+    this.bgSpeed = 2;
+    if (this.skipCount === 0) {
+      this.skipCount = 250;
+      this.bgSpeed = -500;
     }
-
-    draw() {
-        // 绘制图片
-        this.gua.drawImage(this.bg)
-        if (this.bg.y >= this.gua.canvas.height) {
-            this.bg.y = 0
-        }
-        this.gua.drawImage(this.player)
+    for (var i = 0; i < this.bgs.length; i++) {
+      var b = this.bgs[i];
+      b.y += this.bgSpeed;
     }
+  }
 
+  draw() {
+    // 绘制图片
+    this.bgs.forEach((b) => {
+      this.gua.drawImage(b);
+    });
+    this.gua.drawImage(this.player);
+  }
 }
